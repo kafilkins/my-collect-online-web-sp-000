78ed82008df2c8a12933642d4a7f6cd2ab8d25b2
Keith Filkins <kafilkins@me.com>
2020-12-04 19:01:37 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,11 @@
 def my_collect(array)
   i = 0
   collections = []
+  languages = []
+  first_names = []
   while i < collections.length
     collections << yeild(collections[i])
+    languages << yeild(lang.upcase)
     i += 1
   end
   collections
